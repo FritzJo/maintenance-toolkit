b2bc4fe85fa50f28ae1aa5d45efa8d6832a7b0e8
FritzJo <fritzjo-git@mailbox.org>
2019-09-06 00:36:37 +0200

    initial commit

---
+++ update_docker.sh
@@ -0,0 +1,21 @@
+DOCKERPATH=
+CURRENTPATH=$PWD
+
+# Update OS
+apt-get update && apt-get -y upgrade
+apt-get autoremove
+
+# Update docker images
+docker images | grep -v REPOSITORY | awk '{print $1}' | xargs -L1 docker pull
+
+# Reboot every docker-compose file
+cd $DOCKERPATH
+for d in */ ; do
+    echo "$d"
+    cd $d
+    docker-compose down && docker-compose up -d
+    cd ..
+done
+
+cd $CURRENTPATH
+echo "Update completed!"
